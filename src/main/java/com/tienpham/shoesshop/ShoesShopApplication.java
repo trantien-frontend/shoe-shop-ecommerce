9f4829ff76a5004896824f3a47edaf91d874adbe
trantien-frontend <Binh@190998>
2026-01-09 15:55:32 +0700

    init-project

---
+++ src/main/java/com/tienpham/shoesshop/ShoesShopApplication.java
@@ -0,0 +1,13 @@
+package com.tienpham.shoesshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoesShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tienpham/shoesshop/ShoesShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienpham.shoesshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
